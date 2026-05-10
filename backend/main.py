d01d3cfa4fb555078fbcadf2c95e462ccbe9003f
Jia kai. W <wongjiakai@outlook.com>
2026-05-11 00:48:26 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",  # optional for local testing
-        "https://networkintrusiondection.netlify.app"
+        "https://networkintrusiondetection.netlify.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],

4fa18016b7950eab5b827d7b65cb77e9804447aa
Jia kai. W <wongjiakai@outlook.com>
2026-05-11 19:09:08 +0800

    Update CORS origin for backend API

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="IDS Backend API")
 
 origins = [
     "http://localhost:5173",
-    "https://networkintrusiondetection.netlify.app",
+    "https://networkintrusiondetection-system.netlify.app",
 ]
 
 app.add_middleware(
